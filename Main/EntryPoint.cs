48da933715da078560dd357c31a613d01b64d28e
Konstantin Serditov <kserditov@enkata.com>
2014-07-25 18:17:13 +0400

    KSerditov. Added C# project.

---
+++ Main/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Enkata.HostInfrastructure.CustomProjects;
+
+namespace Main
+{
+    public class EntryPoint:ICustomProject2
+    {
+        private OpenSpan1.Project.OpenSpan1 proj = new OpenSpan1.Project.OpenSpan1();
+
+        public System.ComponentModel.Design.IServiceContainer GetRuntimeServiceContainer()
+        {
+            return proj.ServiceContainer;
+        }
+
+        public void Start(Dictionary<string, string> config, Enkata.HostInfrastructure.Messaging.IMessageTransmitter<ActivityTrackerContracts.Messages.EventMessage> messageService)
+        {
+            proj.Start();
+        }
+
+        public void Dispose()
+        {
+            proj.Stop();
+            proj = null;
+        }
+    }
+}
